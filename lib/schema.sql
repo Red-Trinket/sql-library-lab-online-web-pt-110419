fc5848a77b666cd7ef0081f0a70e66f474290172
Matthew Jones <matts3371@gmail.com>
2020-01-25 23:08:32 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Series(
+id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INTEGER,
+sub-genre_id INTEGER 
+);
+
+CREATE TABLE Sub-Genres(
+id INTEGER PRIMARY KEY,
+name TEXT 
+);
+
+CREATE TABLE Authors(
+id INTEGER PRIMARY KEY,
+name TEXT 
+);
+
+CREATE TABLE Books(
+id INTEGER PRIMARY KEY, 
+title TEXT, 
+year INTEGER,
+series_id INTEGER
+);
+
+CREATE TABLE Characters(
+id INTEGER PRIMARY KEY,
+name TEXT, 
+motto TEXT,
+species TEXT,
+author_id INTEGER 
+);
+
+CREATE TABLE character_books (
+id INTEGER PRIMARY KEY,
+
+)
